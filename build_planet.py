Added Cp*T as another energetics paramter to track. Fixed units on some other parameters

build_planet.py
@@ -21,8 +21,7 @@ import burnman.composite as composite
 
 import inspect
 
-from core_partition import w_to_x, x_to_w
-from mercury_minerals import iron_latent_heat
+from core_partition import w_to_x, x_to_w, density_coexist, iron_latent_heat
 
 import mercury_reference as ref
 
@@ -402,17 +401,6 @@ class cm_Planet(object):
             ax1.legend()
             plt.show()
 
-#     def mass_list(self):
-#         '''
-#         Returns a list of masses of the planet [kg]
-#         '''
-#         masses = np.empty( len(self.compositions) )
-#         rhofunc = UnivariateSpline(self.radius, self.density )
-#         for i,layer in enumerate(self.compositions):
-#             masses[i] = integrate.quad( lambda r : 4.0*np.pi*r*r*rhofunc(r) ,
-#                                      (0.0 if i==0 else self.boundaries[i-1]), self.boundaries[i] )[0]
-#         return masses
-  
     def mass(self):
         return self.massBelowBoundary[-1]
   
@@ -477,6 +465,27 @@ class cm_Planet(object):
             C_avg.append(np.mean(C) )
 
         return np.array(C_avg)
+
+    def specific_thermal_energy(self):
+        '''
+        Returns the average Cp * T for each layer. (J/kg)
+        '''
+        CT_avg = []
+        for i,layer in enumerate(self.get_layers()):
+            p_layer = self.pressure[layer]
+            t_layer = self.temperature[layer]
+            phase = self.compositions[i]
+        
+            CT=[]
+            for p,t in zip(p_layer,t_layer):
+                phase.set_state(p,t)
+                try:
+                    CT.append(phase.C_p * phase.molar_mass() * t)
+                except:
+                    CT.append(0.)
+            CT_avg.append(np.mean(CT) )
+
+        return np.array(CT_avg)
 
     def total_thermal_energy(self):
         '''
@@ -568,7 +577,7 @@ class cm_Planet(object):
 
 
 class corePlanet(cm_Planet):
-    def __init__(self,  masses, compositions, temperatures, liquidus=None,**kwargs):
+    def __init__(self,  masses, compositions, temperatures, liquidus=None,materials=None,**kwargs):
         """
         Parameters
         ----------
@@ -592,6 +601,8 @@ class corePlanet(cm_Planet):
 
         # liquidus model
         self.liquidus_model = liquidus()
+
+        self.materials = materials
 
         # Make sure the number of layers is consistent with having a growing core
         assert self.Nlayer >= 3
@@ -715,17 +726,21 @@ class corePlanet(cm_Planet):
         p_icb = self.pressure[idx_icb]
         rho_icb = self.density[idx_icb]
         g_icb = self.gravity[idx_icb]
-        r_icb = self.radius[idx_icb]
+        r_icb = self.boundaries[0]
         t_icb = self.temperature[idx_icb]
 
-        rho_s,vp, vs, vphi, K, G =  burnman.velocities_from_rock(self.compositions[0],
-                np.array([p_icb]), np.array([t_icb]) )
+        # is this necessary? or is taking rho_ic[-1] - rho_oc[0] sufficient?
+#         rho_s,vp, vs, vphi, K, G =  burnman.velocities_from_rock(self.compositions[0],
+#                 np.array([p_icb]), np.array([t_icb]) )
+# 
+#         rho_l, vp, vs, vphi, K, G = burnman.velocities_from_rock(self.compositions[1],
+#                 np.array([p_icb]), np.array([t_icb]) )
 
-        rho_l, vp, vs, vphi, K, G = burnman.velocities_from_rock(self.compositions[1],
-                np.array([p_icb]), np.array([t_icb]) )
+        rho_s,rho_l = density_coexist(self.w_l,[self.DS,self.DSi],p_icb,t_icb,\
+                self.materials[0],self.materials[1])
 
         # return Eg / dr
-        return (rho_l - rho_s) * g_icb * 4. * np.pi * r_icb**2.
+        return (rho_s - rho_l) * g_icb * r_icb * 4. * np.pi * r_icb**2.
 
     def specific_gravitational_energy(self):
         '''
@@ -742,7 +757,7 @@ class corePlanet(cm_Planet):
         '''
         p = self.pressure[self.icb()]
         t = self.temperature[self.icb()]
-        rho = self.density[self.icb()]
+        rho = self.density[self.inner_core()][-1]
         r = self.boundaries[0]
         dm_dr = rho * 4. * np.pi * r**2
         return iron_latent_heat(p,t,self.w_l) * dm_dr

mercury_interior_structure_model.py
@@ -66,9 +66,41 @@ class mercuryModel(corePlanet):
         masses = [ 0., self.M_core, self.M_mantle]
         compositions = [None,None,None]
 
+        materials = [ironSilicideAlloy,ironSulfideSilicideLiquid,\
+                [olivine,orthopyroxene]]
+
         # build planet!
         super(mercuryModel,self).__init__(masses, compositions,T0,\
-                liquidus=liquidus, **kwargs )
+                liquidus=liquidus,materials=materials, **kwargs )
+
+    def set_compositions(self,**kwargs):
+        '''
+        Set compositions (burnman.Material instances) corresponding to
+        x_l, x_s and mantle mineral parameters defined in mercury_minerals.
+        '''
+
+        #mantle minerals
+        n_fe_ol = ref.n_fe_ol # iron content of mantle minerals
+        n_fe_opx = ref.n_fe_opx
+        ol = olivine(n_fe_ol)
+        opx = orthopyroxene(n_fe_opx)
+
+        # fraction of olivine and orthopyroxene in the mantle
+        fol = ref.fol; fopx = ref.fopx
+        rock = burnman.Composite([fol,fopx],[ol,opx])
+
+        # liquid outer core
+        liquidFeSSi = ironSulfideSilicideLiquid(self.x_l[0],self.x_l[1]) # ternary solution
+
+        # solid inner core
+        solidFeSi = ironSilicideAlloy(self.x_s[1]) # solid solution of Si in Fe
+
+        # set materials for each layer
+        self.materials = [ironSilicideAlloy,ironSulfideSilicideLiquid,\
+                [olivine,orthopyroxene]]
+        
+        # set self.compositions and set methods
+        super(mercuryModel,self).set_compositions([solidFeSi,liquidFeSSi,rock])
 
     def set_innerCore(self,inner_Mfrac):
         '''
@@ -88,16 +120,6 @@ class mercuryModel(corePlanet):
         self.masses = [self.M_inner,self.M_outer,self.M_mantle]
         self.update_massBelowBoundary()
 
-        #mantle minerals
-        n_fe_ol = ref.n_fe_ol # iron content of mantle minerals
-        n_fe_opx = ref.n_fe_opx
-        ol = olivine(n_fe_ol)
-        opx = orthopyroxene(n_fe_opx)
-
-        # fraction of olivine and orthopyroxene in the mantle
-        fol = ref.fol; fopx = ref.fopx
-        rock = burnman.Composite([fol,fopx],[ol,opx])
-
         # Distribution coefficients [Wsolid]/[Wliquid] (Is this correct, the different
         # weight percents dont take echother into account).
         self.DS = ref.DS 
@@ -113,7 +135,6 @@ class mercuryModel(corePlanet):
         assert np.sum(self.x_l) == 1.
         assert np.all(self.x_l >= 0.)
 
-        liquidFeSSi = ironSulfideSilicideLiquid(self.x_l[0],self.x_l[1]) # ternary solution
 
         self.w_s = np.array([ w_inner[0],w_inner[1],1.-w_inner[0]-w_inner[1]] )
         self.x_s = w_to_x(self.w_s)
@@ -124,10 +145,8 @@ class mercuryModel(corePlanet):
         assert np.sum(self.x_s) == 1.
         assert np.all(self.x_s >= 0.)
 
-        solidFeSi = ironSilicideAlloy(self.x_s[1]) # solid solution of Si in Fe
-
         # set compositions to the burnman minerals of corresponding composition
-        self.set_compositions([solidFeSi,liquidFeSSi,rock])
+        self.set_compositions()
 
         # build planet!
 #         self.planet = corePlanet([self.M_inner,self.M_outer,self.M_mantle],
@@ -203,14 +222,48 @@ class mercuryModel(corePlanet):
 
 class model_suite(object):
     def __init__(self,planet,inner_Mfracs,**kwargs):
+        '''
+        Definte a model of a planet to determine energetics of core growth.
+        -----------------------------------------------------------------------
+        args:
+
+            planet: corePlanet or mercuryModel object
+
+            inner_Mfracs : array of mass fractions of inner core (0,1)
+
+        -----------------------------------------------------------------------
+        '''
         self.inner_Mfracs = inner_Mfracs
         self.planet = planet
     def get_energetics(self,**kwargs):
+        '''
+        Tabulates quantities for use with parameterized convection code:
+
+        -----------------------------------------------------------------------
+        Quantaties:
+
+            'm_frac' : Fraction of the core mass in the solid inner core.
+            'r_frac' : Fraction of the core radius in the solid inner core.
+            'r_icb', 'r_cmb', 'r_surf' : boundary radii (m)
+            'T_icb', 'T_cmb' : boundary temperature (K)
+            'T_avg_ic', 'T_av_oc' : mass averaged temperature (K)
+            'Eg_r': Gravitational energy release per change in inner core 
+                    radius (J/m)
+            'L_r' : Latent heat release per change in inner core radius (J/m)
+            'Eg_m' : Gravitational energy release per change inner core 
+                    mass (J/kg)
+            'L_m' : Latent heat release per change in inner core mass (J/kg)
+            'Cp_ic','Cp_oc' : Average specific heat capcity for each 
+                    layer (J/K/kg)
+            'CpT_avg_ic','CpT_avg_oc': Average Cp*T for each layer (J/K)
+            'm_ic','m_oc': mass of inner and outer core (kg)
+        '''
 
         row_list = []
         at_eutectic = False
         self.labels = ['m_frac','r_frac','r_icb','r_cmb','r_surf','T_icb','T_cmb',\
-                'T_avg_ic','T_av_oc','Eg_r','L_r','Eg_m','L_m','Cp_ic','Cp_oc']
+                'T_avg_ic','T_av_oc','Eg_r','L_r','Eg_m','L_m','Cp_ic','Cp_oc',\
+                'CpT_avg_ic','CpT_avg_oc','m_ic','m_oc']
         for mfrac in self.inner_Mfracs:
 
             print 'Core mass fraction:', mfrac
@@ -219,40 +272,42 @@ class model_suite(object):
                 print 'Eutectic encountered'
                 break
 
-#             try:
-            self.planet.set_innerCore(mfrac) 
+            try:
+                self.planet.set_innerCore(mfrac) 
 
-            self.planet.integrate(verbose=False,**kwargs)
-
-
-            T_icb = self.planet.boundary_temperatures[0]
-            T_cmb = self.planet.boundary_temperatures[1]
-
-            m_ic = self.planet.masses[0]
-            m_oc = self.planet.masses[1]
-
-            r_icb = self.planet.boundaries[0]
-            r_cmb = self.planet.boundaries[1]
-            r_surf = self.planet.boundaries[-1]
-            rfrac = r_icb / r_cmb
-
-            Cp_avg = self.planet.average_heat_capacity()
-            T_avg = self.planet.average_temperature()
-
-            Eg_m = self.planet.specific_gravitational_energy()
-            Eg_r = self.planet.gravitational_energy_over_r()
-
-            L_r = self.planet.latent_heat_over_r()
-            L_m = self.planet.specific_latent_heat()
+                self.planet.integrate(verbose=False,**kwargs)
 
 
-            row = np.array([mfrac,rfrac,r_icb,r_cmb,r_surf,T_icb,T_cmb,\
-                    T_avg[0],T_avg[1],Eg_r,L_r,Eg_m,L_m,Cp_avg[0],Cp_avg[1]])
-            print row
-                
-            row_list.append(row)
-#             except:
-#                 print 'Problem encountered, skipping step without adding data'
+                T_icb = self.planet.boundary_temperatures[0]
+                T_cmb = self.planet.boundary_temperatures[1]
+
+                m_ic = self.planet.masses[0]
+                m_oc = self.planet.masses[1]
+
+                r_icb = self.planet.boundaries[0]
+                r_cmb = self.planet.boundaries[1]
+                r_surf = self.planet.boundaries[-1]
+                rfrac = r_icb / r_cmb
+
+                Cp_avg = self.planet.average_heat_capacity()
+                T_avg = self.planet.average_temperature()
+                CpT_avg = self.planet.specific_thermal_energy()
+
+                Eg_m = self.planet.specific_gravitational_energy()
+                Eg_r = self.planet.gravitational_energy_over_r()
+
+                L_r = self.planet.latent_heat_over_r()
+                L_m = self.planet.specific_latent_heat()
+
+
+                row = np.array([mfrac,rfrac,r_icb,r_cmb,r_surf,T_icb,T_cmb,\
+                        T_avg[0],T_avg[1],Eg_r,L_r,Eg_m,L_m,Cp_avg[0],Cp_avg[1],\
+                        CpT_avg[0],CpT_avg[1],m_ic,m_oc])
+                print row
+                    
+                row_list.append(row)
+            except:
+                print 'Problem encountered, skipping step without adding data'
 
         print row_list
         self.data = pd.DataFrame(row_list)
@@ -261,24 +316,55 @@ class model_suite(object):
         self.T_min = self.data.T_icb.min()
         
     def saveData(self,file_name):
+        '''
+        Save tabulated data for a set of runs to a file
+        '''
         self.data.save(file_name)
     def loadData(self,file_name):
+        '''
+        Load tabulated data for a set of runs from a file
+        '''
         pd.load(file_name)
     def printData(self):
         print self.data
     def func_of_Ticb(self,label):
-        y = self.data[label]
-        x = self.data.T_icb
+        '''
+        Fit a function as of a given quantity w. r. t. the inner core
+        boundary temperature.
+        '''
+        y = self.data[label][::-1] 
+        x = self.data.T_icb[::-1] # Note x must be increasing for Univariatespline
 
         return UnivariateSpline(x,y)
+    
+    def func_of_data(self,xlabel,ylabel):
+        if self.data[xlabel][0] > self.data[xlabel][-1]:
+            y = self.data[ylabel][::-1] 
+            x = self.data[xlabel::-1] # Note x must be increasing for Univariatespline
+        else:
+            y = self.data[ylabel]
+            x = self.data[xlabel]
+
+        return UnivariateSpline(x,y)
+
+    def thermal_energy_change(self):
+        m_ic_func = self.func_of_Ticb('m_ic')
+        m_oc_func = self.func_of_Ticb('m_oc')
+        CpT_avg_ic_func = self.func_of_Ticb('CpT_avg_ic')
+        CpT_avg_oc_func = self.func_of_Ticb('CpT_avg_oc')
+        Eth_ic = lambda t : derivative(CpT_avg_ic_func,t) * m_ic_func(t)
+        Eth_oc = lambda t : derivative(CpT_avg_ic_func,t) * m_oc_func(t)
+        return Eth_ic, Eth_oc
 
 if __name__ == "__main__":
     # .58,.68,.63 (range in masses found in Hauck)
     merc = mercuryModel(0.63,.00,.00)
 
+#     ### Test 1: Look at profiles and determine whether snow predicted
 #     merc.generate_profiles(0.5)
 #     merc.show_profiles()
 
+    ### Test 2: Tabulate and plot energetics for a mercuryModel
     model1 = model_suite(merc,[0.1,0.2,0.3,0.4,0.5])
     model1.get_energetics()
     model1.printData()
@@ -292,6 +378,8 @@ if __name__ == "__main__":
     Eg_m_func = model1.func_of_Ticb('Eg_m')
     L_m_func = model1.func_of_Ticb('L_m')
 
+    dEth_ic, dEth_oc = model1.thermal_energy_change()
+
     t = np.linspace(model1.T_min,model1.T_max,50)
 
     f1 = plt.figure()
@@ -303,11 +391,24 @@ if __name__ == "__main__":
     ax2 = plt.subplot(111)
     ax2.plot(t,Eg_r_func(t))
     ax2.plot(t,L_r_func(t))
+    ax2.plot(t,dEth_ic(t))
+    ax2.plot(t,dEth_oc(t))
 
     f3 = plt.figure()
     ax3 = plt.subplot(111)
     ax3.plot(t,Eg_m_func(t))
     ax3.plot(t,L_m_func(t))
 
+#     f4 = plt.figure()
+#     ax4 = plt.subplot(111)
+#     ax4.plot(t,model1.func_of_Ticb('CpT_avg_ic')(t))
+#     ax4.plot(t,model1.func_of_Ticb('CpT_avg_oc')(t))
+# 
+#     f4 = plt.figure()
+#     ax4 = plt.subplot(111)
+#     ax4.plot(t,derivative(model1.func_of_Ticb('CpT_avg_ic'),t))
+#     ax4.plot(t,derivative(model1.func_of_Ticb('CpT_avg_oc'),t))
+
     plt.show()
 
+    ### Test 3: Plot dT / dP a la Williams ( )

mercury_minerals.py
@@ -2,6 +2,8 @@
 mercury_minerals.py
 
 Contains model material properties for a mercurian mantle and core.
+
+Minerals are define using the
 '''
 
 import burnman
@@ -13,10 +15,8 @@ from scipy.interpolate import UnivariateSpline
 from core_partition import x_to_w, w_to_x
 
 # molar masses of elements
-from mercury_reference import mFe,mSi,mS,LFe
+from mercury_reference import mFe,mSi,mS
 
-# iron latent heat of fusion
-iron_latent_heat = lambda P, T, w : LFe
 
 # Core Material Properties
 class gamma_iron(burnman.Mineral):