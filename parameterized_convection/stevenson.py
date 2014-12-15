2f20bf1115d5fa0bde2cc6cfb977ec9736e00f82
bgd88 <delbridge@berkeley.edu>
2014-12-14 21:59:27 -0800

    Couples Stevenson to Sean's Code

--- parameterized_convection/stevenson.py
+++ parameterized_convection/stevenson.py
@@ -6,12 +6,17 @@ import numpy as np
 import scipy.integrate as integrate
 import scipy.optimize as opt
 from scipy.misc import derivative
-
+from mercury_parameters import rho_core, core_heat_capacity
 import planetary_energetics
-
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join('.', os.pardir)))
+from mercury_interior_structure_model import model1
+from scipy.constants import Julian_year
+import prettyplotlib as ppl
 
 class StevensonCoreLayer(planetary_energetics.Layer):
-    def __init__(self,inner_radius,outer_radius, params={}):
+    def __init__(self, inner_radius, outer_radius, params={}):
         planetary_energetics.Layer.__init__(self,inner_radius,outer_radius,params)
         '''
         Note that the default params are loaded from the file "define_physics"
@@ -113,7 +118,36 @@ class StevensonCoreLayer(planetary_energetics.Layer):
         latent_heat = -p['L+Eg'] * p['rho'] * inner_core_surface_area * dRi_dTcmb
         print (thermal_energy_change-latent_heat)
         dTdt = -core_flux * core_surface_area / (thermal_energy_change-latent_heat)
+        print "dTc_dt :", dTdt
+        print "Left Hand Side", (thermal_energy_change-latent_heat)
         return dTdt
+
+class realisticCoreLayer(planetary_energetics.Layer):
+    def __init__(self,inner_radius,outer_radius, core_energetic_model, params={}):
+        planetary_energetics.Layer.__init__(self, inner_radius,outer_radius,params)
+        self.core_energetic_model = core_energetic_model
+        self.density = params['density']
+        self.heat_capacity = params['heat_capacity']
+
+    def energy_balance(self, T_cmb, core_flux):
+        thermal_energy_change, gravitational_energy_release, latent_heat, total_effective_heat_capacity,\
+            radius_inner_core = self.core_energetic_model.get_effective_core_heat_capacity()
+
+        lhs = self.density*self.heat_capacity*(4./3.*np.pi*self.volume) -\
+            gravitational_energy_release(T_cmb) - latent_heat(T_cmb)
+        if(lhs<0):
+            print "I am the lhs: ", lhs
+            print  thermal_energy_change(T_cmb), gravitational_energy_release(T_cmb), latent_heat(T_cmb)
+        dTc_dt = -100.*core_flux*self.outer_surface_area/lhs
+        #print "dTc_dt", dTc_dt
+        #print "Effective Heat Capacity: ", lhs
+        assert(dTc_dt<0)
+        return dTc_dt
+
+    def inner_radius(self, T_upper_mantle):
+        thermal_energy_change, gravitational_energy_release, latent_heat, total_effective_heat_capacity,\
+            radius_inner_core = self.core_energetic_model.get_effective_core_heat_capacity()
+        return radius_inner_core(T_upper_mantle)
 
 
 class StevensonMantleLayer(planetary_energetics.Layer):
@@ -173,7 +207,9 @@ class StevensonMantleLayer(planetary_energetics.Layer):
         the top of the lower boundary layer assuming negligable boundary layer thickness.
         '''
         p =self.stevenson
-        return T_upper_mantle*( 1.0 + p['alpha']*p['g']*self.thickness/p['c'])
+        temperature_lower_mantle = T_upper_mantle*( 1.0 + p['alpha']*p['g']*self.thickness/p['c'])
+  #      print "T_upper_mantle :", temperature_lower_mantle
+        return temperature_lower_mantle
     
     def mantle_rayleigh_number(self, T_upper_mantle, T_cmb):
         '''
@@ -181,20 +217,26 @@ class StevensonMantleLayer(planetary_energetics.Layer):
         '''
         p = self.stevenson
         nu = self.kinematic_viscosity(T_upper_mantle)
-        T_lower_mantle = self.lower_mantle_temperature(T_upper_mantle)
+        T_lower_mantle = T_upper_mantle#self.lower_mantle_temperature(T_upper_mantle)
         upper_boundary_delta_T = T_upper_mantle - self.surface_temperature
         lower_boundary_delta_T = T_cmb - T_lower_mantle
         assert( upper_boundary_delta_T > 0.0)
         assert( lower_boundary_delta_T > 0.0)
         delta_T_effective = upper_boundary_delta_T + lower_boundary_delta_T
-        return p['g']*p['alpha']*( delta_T_effective)*np.power(self.thickness,3.)/(nu*p['K_diff'])
+        Ra = p['g']*p['alpha']*( delta_T_effective)*np.power(self.thickness,3.)/(nu*p['K_diff'])
+ #       print "Rayleigh Number :", Ra
+        assert(Ra>0.0)
+        return Ra
     
     def boundary_layer_thickness(self, Ra_mantle):
         '''
         Equation (18) Stevenson et al 1983
         '''
         p = self.stevenson
-        return self.thickness*np.power(p['Ra_crit']/Ra_mantle,p['beta'])
+        boundary_layer_thickness = self.thickness*np.power(p['Ra_crit']/Ra_mantle,p['beta'])
+#        print "Boundary Layer Thickness :", boundary_layer_thickness
+        assert(boundary_layer_thickness>0)
+        return boundary_layer_thickness
 
     def upper_boundary_layer_thickness(self, T_upper_mantle, T_cmb):
         '''
@@ -209,7 +251,7 @@ class StevensonMantleLayer(planetary_energetics.Layer):
         '''
         p = self.stevenson
         T_lower_mantle = self.lower_mantle_temperature(T_upper_mantle)
-        average_boundary_layer_temp = (T_upper_mantle + T_lower_mantle)/2
+        average_boundary_layer_temp = (T_cmb + T_lower_mantle)/2
         nu_crit = self.kinematic_viscosity(average_boundary_layer_temp)
         delta_T_lower_boundary_layer = T_cmb - T_lower_mantle
         assert( delta_T_lower_boundary_layer > 0.0 )
@@ -243,10 +285,76 @@ class StevensonMantleLayer(planetary_energetics.Layer):
         return dTdt
 
 
-mercury = planetary_energetics.Planet( [ StevensonCoreLayer( 0.0, 2020.0e3) , StevensonMantleLayer( 2020.e3, 2440.e3 ) ] )
-t, y = mercury.integrate()
-mercury.draw()
-plt.plot( t, y[:,0])
-plt.plot( t, y[:,1])
-plt.show()
+#mercury = planetary_energetics.Planet( [ StevensonCoreLayer( 0.0, 2020.0e3) , StevensonMantleLayer( 2020.e3, 2440.e3 ) ] )
+mercury = planetary_energetics.Planet( [ realisticCoreLayer( 0.0, 2020.0e3, model1, {'density': rho_core, 'heat_capacity': core_heat_capacity}) , StevensonMantleLayer( 2020.e3, 2440.e3 ) ] )
+times = np.linspace( 0., Julian_year*np.pi*1.e7)
+
+T_excess = 800.
+times = np.linspace(0.0, 4.5*1.e9*Julian_year)
+T_mantle_initial = 1800.
+
+
+T_cmb_initial = T_mantle_initial + T_excess
+Tm_low = T_mantle_initial - 100
+Tcmb_low = T_cmb_initial - 100
+Tm_high = T_mantle_initial + 100
+Tcmb_high = T_cmb_initial + 100
+
+t, y_low = mercury.integrate(Tcmb_low, Tm_low, times)
+t, y_high = mercury.integrate(Tcmb_high, Tm_high, times)
+t, y = mercury.integrate(T_cmb_initial, T_mantle_initial, times)
+t = t/1.e9*Julian_year;
+c = [ppl.colors.set2[0], ppl.colors.set2[1]]
+plt.figure()
+ppl.fill_between(t, y_low[:,0], y_high[:,0], color=c[0], label=r'CMB Temperature \pm 100 K')
+plt.plot(t, y_low[:,0],'--k')
+plt.plot(t, y_high[:,0],'--k')
+ppl.fill_between(t, y_low[:,1], y_high[:,1], color=c[1], label=r'Upper Mantle Temperature \pm 100 K')
+plt.plot(t, y_low[:,1],'--k')
+plt.plot(t, y_high[:,1],'--k')
+#mercury.draw()
+
+ppl.plot( t, y[:,0], lw =3, color=c[0], label = 'CMB Temperature')
+ppl.plot( t, y[:,0],  '--k',lw=1)
+ppl.plot( t, y[:,1],  lw=3, color=c[1], label = 'Upper Mantle Temperature')
+ppl.plot( t, y[:,1],  '--k',lw=1)
+plt.xlabel('Time [Ga]')
+plt.ylabel('Temperature [K]')
+plt.title("Thermal Evolution Mercury")
+ppl.legend(loc=1)
+plt.savefig('thermal_evolution.png')
+
+thermal_energy_change, gravitational_energy_release, latent_heat, total_effective_heat_capacity,\
+            radius_inner_core = model1.get_effective_core_heat_capacity()
+grav = []
+total = []
+latent = []
+radius = []
+for temp in y[:,1]:
+    radius.append(radius_inner_core(temp)/1000.)
+    grav.append(-gravitational_energy_release(temp)*1.e-26)
+    #print "LATENT", latent_heat(temp)
+    #print "Grav", gravitational_energy_release(temp)
+    latent.append(-latent_heat(temp)*1.e-26)
+    total.append(-total_effective_heat_capacity(temp)*1.e-26)
+
+plt.figure()
+ppl.plot(t,radius, label="Inner Core Radius")
+plt.xlabel('Time [Ga]')
+plt.ylabel('Inner Core Radius [kg]')
+plt.title("Inner Core Growth")
+ppl.legend()
+plt.savefig('inner_core_growth.png')
+
+plt.figure()
+ppl.plot(t,grav,  label="Gravitational Energy Release")
+ppl.plot(t,latent, label="Latent Heat Release")
+ppl.plot(t,total, label="Total Effective Heat Capacity")
+plt.xlabel('Time [Ga]')
+plt.ylabel(r'\frac{dE}{dT_cmb} [W/K] * 10^{-26}')
+plt.title(r'Core Energetics')
+ppl.legend()
+plt.savefig('Core Energetics')
+
+
 
